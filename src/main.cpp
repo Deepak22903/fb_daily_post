ddfc0a0c9ec90a188806861c2ce137332995891a
Deepak22903 <deepakshitole4@gmail.com>
2025-02-15 22:53:33 +0530

    added user input

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,31 @@
 
 int main() {
   load_env_vars();
-  family_island();
-  coin_tales();
+  int choice;
+  cout << "\n ***************************** FACEBOOK AUTO POST "
+          "*******************************\n\n";
+  cout << "Enter Ctrl+c to exit...\n\n";
+  while (1) {
+    cout << " 1.All Pages \n 2.Select Pages\n >>";
+    cin >> choice;
+    if (choice == 1) {
+      family_island();
+      coin_tales();
+    } else if (choice == 2) {
+      int opt;
+      cout << "\n 1.Family Island\n 2.Coin Tales\n >>";
+      cin >> opt;
+      if (opt == 1) {
+        family_island();
+      } else if (opt == 2) {
+        coin_tales();
+      } else {
+        cout << "Enter valid page!" << endl;
+      }
+    } else {
+      cout << "Enter a valid input!" << endl;
+    }
+  }
   // string img =
   //     generateImageFromImage("./output/test.jpg", "generate the similar
   //     image");
